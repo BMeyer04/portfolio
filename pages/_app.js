c01b7b7b191a77c810a36f0fcac189024380af6a
BMeyer04 <117365123+BMeyer04@users.noreply.github.com>
2022-11-07 13:46:07 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ export default function Nextra({ Component, pageProps }) {
         <link
           rel="alternate"
           type="application/rss+xml"
-          title="RSS"
+          title="Brandon Meyer"
           href="/feed.xml"
         />
         <link
