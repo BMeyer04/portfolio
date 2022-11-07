02bb6860626476222f2f1e615700f3f17d6bbdc2
BMeyer04 <117365123+BMeyer04@users.noreply.github.com>
2022-11-07 13:28:16 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ export default function Nextra({ Component, pageProps }) {
         <link
           rel="alternate"
           type="application/rss+xml"
-          title="RSS"
+          title="my app"
           href="/feed.xml"
         />
         <link
